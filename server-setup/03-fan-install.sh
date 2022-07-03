ba75b54e52a9e07c17d3f70d22b9bf6b53b37bd7
btk44 <97596263+btk44@users.noreply.github.com>
2022-07-03 08:37:13 +0200

    Update 03-fan-install.sh

--- server-setup/03-fan-install.sh
+++ server-setup/03-fan-install.sh
@@ -1,6 +1,6 @@
-cd ~/server-setup/fanshim-python
+cd fanshim-python
 sudo ./install.sh
 cd examples
 sudo apt-get install python3-pip -y
+
 sudo ./install-service.sh --on-threshold 65 --off-threshold 55 --delay 2 --brightness 5
-cd ~/
