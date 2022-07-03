a61ba73bae6c224a21f0d69af08ea16f83347c59
btk44 <97596263+btk44@users.noreply.github.com>
2022-07-03 08:36:02 +0200

    Update 02-repos-git-pull.sh

--- server-setup/02-repos-git-pull.sh
+++ server-setup/02-repos-git-pull.sh
@@ -1,6 +1,6 @@
-cd ~/server-setup
+# pi-hosted - RPi server with docker self-hosted apps
 git clone https://github.com/pi-hosted/pi-hosted.git
 
-cd ~/server-setup
+# installation scripts for RPi fan from Pimoroni
 git clone https://github.com/pimoroni/fanshim-python
-cd ~/
+
