25545e273a9364eb36a5989241b856aa4e32db3e
btk44 <97596263+btk44@users.noreply.github.com>
2022-12-23 12:11:13 +0100

    add parameters to backup srcipt

--- backup-setup/backup_action.sh
+++ backup-setup/backup_action.sh
@@ -7,12 +7,23 @@ backup_target=$backup_drive_mount/subfolder/in/mounted/drive
 backup_log=/path/to/your/logfile.txt
 now=$(date)
 
-echo "\n\n--------------------------------------------------------\nLog from date: $now :\n" >> $backup_log
+case $1 in
 
-sudo mount UUID=$backup_drive_uuid $backup_drive_mount >> $backup_log # this command sometimes fails (very rare) and bloks ssh
-sleep 30
-sudo rsync -avh --delete $backup_source $backup_target >> $backup_log 
-sudo umount $backup_drive_mount >> $backup_log
+  m) # run: "sh backup_action.sh m"
+    sudo mount UUID=$backup_drive_uuid $backup_drive_mount >> $backup_log # this command sometimes fails (very rare) and bloks ssh
+    ;;
 
-echo "\n--------------------------------------------------------" >> $backup_log
+  u) # run: "sh backup_action.sh u"
+    sudo umount $backup_drive_mount >> $backup_log
+    ;;
 
+  b) # run: "sh backup_action.sh b"
+    echo "\n\n--------------------------------------------------------\nLog from date: $now :\n" >> $backup_log
+    sudo rsync -avh --delete $backup_source $backup_target >> $backup_log
+    echo "\n--------------------------------------------------------" >> $backup_log
+    ;;
+
+  *)
+    echo "unknown action"
+    ;;
+esac
